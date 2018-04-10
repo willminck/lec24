ca78425acf93439696577134c5c5230320775a95
Will Minck <willminck@0587375511.wireless.umich.net>
2018-04-10 16:55:21 -0400

    Done with Lecture Exercise 24

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import model
 
 app = Flask(__name__)
@@ -9,13 +9,27 @@ def index():
         <img src="/static/blockM.png"/>
         <h1>Michigan Sports Info!</h1>
         <ul>
-            <li><a href="/bball"> Men's Basketball </a></li>
+            <li><a href="/fball"> Men's Football </a></li>
         </ul>
     '''
 
-@app.route('/bball')
-def bball():
-    return render_template("seasons.html", seasons=model.get_bball_seasons())
+@app.route('/fball', methods=['GET', 'POST'])
+def fball():
+    if request.method == 'POST':
+        sortby = request.form['sortby']
+        sortorder = request.form['sortorder']
+        seasons = model.get_fball_seasons(sortby, sortorder)
+    else:
+        seasons = model.get_fball_seasons()
+
+    return render_template("seasons.html", seasons=seasons)
+
+@app.route('/hello', methods=['GET', 'POST'])
+def hello():
+    firstname = request.form['firstname']
+    lastname = request.form['lastname']
+    return render_template("hello.html", firstname=firstname, lastname=lastname)
 
 if __name__ == '__main__':
+    model.init_fball()
     app.run(debug=True)
--- model.py
+++ model.py
@@ -1,13 +1,34 @@
 #model.py
 import csv
 
-BB_FILE_NAME = 'umbball.csv'
+FB_FILE_NAME = 'umfootball.csv'
 
-bb_seasons = []
+fb_seasons = []
 
-def init_bball(csv_file_name=BB_FILE_NAME):
-    pass
+def init_fball(csv_file_name=FB_FILE_NAME):
+    global fb_seasons
+    with open(csv_file_name) as f:
+        reader = csv.reader(f)
+        next(reader) # throw away headers
+        next(reader) # throw away headers
+        global fb_seasons
+        fb_seasons = [] # reset, start clean
+        for r in reader:
+            r[3] = int(r[3])
+            r[4] = int(r[4])
+            r[5] = float(r[5])
+            fb_seasons.append(r)
 
-def get_bball_seasons(sortby='year', sortorder='desc'):
-    global bb_seasons
-    return bb_seasons
+def get_fball_seasons(sortby='year', sortorder='desc'):
+    if sortby == 'year':
+        sortcol = 1
+    elif sortby == 'wins':
+        sortcol = 3
+    elif sortby == 'pct':
+        sortcol = 5
+    else:
+        sortcol = 0
+
+    rev = (sortorder == 'desc')
+    sorted_list = sorted(fb_seasons, key=lambda row: row[sortcol], reverse=rev)
+    return sorted_list
